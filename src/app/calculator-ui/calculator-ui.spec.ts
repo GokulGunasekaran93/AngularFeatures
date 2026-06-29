d690dc22737d964ca3a94173a61f6d4f28b048f9
GokulGunasekaran93 <gokulgunasekaran@outlook.com>
2026-06-29 23:35:23 +0530

    Lesson9 - Sample Unit testing

--- src/app/calculator-ui/calculator-ui.spec.ts
+++ src/app/calculator-ui/calculator-ui.spec.ts
@@ -19,4 +19,15 @@ describe('CalculatorUI', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // write test case for addition operation
+  it('should perform addition operation', () => {
+    component.currentInput = '2+3';
+    component.calculate();
+    expect(component.currentInput).toEqual('5');
+  });
+
+  // how to run only this component test case
+  // npx ng test --include src/app/calculator-ui/calculator-ui.spec.ts
+
 });
